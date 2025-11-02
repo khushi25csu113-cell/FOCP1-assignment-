8574304e4c7d24eb478fe95f6b033cbea5d27fdf
khushi25csu113-cell <khushi25csu113@ncuindia.edu>
2025-11-02 18:13:37 +0530

    check no. of prime numbers in array Q14.c

---
+++ Q14.c
@@ -0,0 +1,10 @@
+#includee<stdio.h>
+int main(){
+  int i,count=0;
+  int pnum[]={23,45,1,3,17,23,44,51};
+  int length=sizeof(pnum)/sizeof(pnum[0]);
+  for(i=0;i<length;i++)
+    {
+      if(pnum
+    }
+}
