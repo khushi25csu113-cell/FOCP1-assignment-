b24254bb861702d8afd84185f9a3db1ae1309457
khushi25csu113-cell <khushi25csu113@ncuindia.edu>
2025-11-02 21:29:54 +0530

    no. of prime numbers
    Fix include directive and correct prime checking logic.

--- Q14.c
+++ Q14.c
@@ -1,10 +1,20 @@
-#includee<stdio.h>
+#include<stdio.h>
+int isprime(int n){
+if(n<2) return 0;
+for(i=2;i<n;i++){
+  if(n%i==0) return 0;
+       }
+return 1;
+}
 int main(){
   int i,count=0;
   int pnum[]={23,45,1,3,17,23,44,51};
   int length=sizeof(pnum)/sizeof(pnum[0]);
   for(i=0;i<length;i++)
     {
-      if(pnum
+      if(isprime(pnum[i]))
+        count++;
     }
+  printf("total prime numbers is %d",count);
+  return 0;
 }
